66330da9163e1875b51354199629ddb79dd108ac
redakeyboardboy <redakeyboardboy@gmail.com>
2022-02-03 16:11:14 +0100

    score -> cups relation

--- OOP/oop.php
+++ OOP/oop.php
@@ -68,7 +68,7 @@ class member{
 
 class coach extends member{
     private $experience;
-    private $cups;
+    private $cups=0;
 
     public function __construct($ID,$name,$salary,$experience,$cups){
       parent::__construct($ID,$name,$salary);
@@ -97,12 +97,15 @@ class player extends member{
     private $number;
     private $height;
     private $age;
-
-    public function __construct($ID,$name,$salary,$number,$height,$age){
+    public $goals = 0;
+    public $coach = null;
+ 
+    public function __construct($ID,$name,$salary,$number,$height,$age,$coach){
       parent::__construct($ID,$name,$salary);
       $this->number = $number;
       $this->height = $height;
       $this->age = $age;
+      $this->coach = $coach;
     }
 
     public function get_number(){
@@ -128,9 +131,38 @@ class player extends member{
    public function set_age($age){
         $this->age = $age;
    }
+   
+   public function score(){
+      $this->goals++;
+      if($this->goals == 10){
+         $cups_won = $this->coach->get_cups();
+         $cups_won++;
+         $this->coach->set_cups($cups_won);
+         $this->goals = 0;
+      }
+   }
 
 }
 
-$player1 = new player(798,"adama",12000,11,1.78,26);
+$coach2 = new coach(998,"koeman",72900,7.2,3);
+$coach1 = new coach(576,"xavi",40000,6.9,0);
+$player1 = new player(798,"adama traore",12000,11,1.78,26,$coach1);
+$player2 = new player(138,"pedri gonzales",46600,16,1.74,19,$coach2);
 
+
+
+   for($i=0;$i<20;$i++){
+      $player1->score();
+   }
+
+   for($i=0;$i<30;$i++){
+      $player2->score();
+   }
+   
+   
+   // printf("XAVI : ".$coach1->get_cups()."\n");
+   // print("KOEMAN : ".$coach2->get_cups());
+   echo '<pre>';
+   print_r($coach1);
+   print_r($coach2);
 ?>
